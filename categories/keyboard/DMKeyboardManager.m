874ccd19566ca8e2509bb2d9f1b42aa1424a5ec6
anna-dev <aabushmeleva@gmail.com>
2017-05-17 13:37:52 +0500

    DMKeyboardManager: UITextFieldTextDidBeginEditingNotification  + resignKeyboard

--- categories/keyboard/DMKeyboardManager.m
+++ categories/keyboard/DMKeyboardManager.m
@@ -8,6 +8,12 @@
 
 #import "DMKeyboardManager.h"
 
+@interface DMKeyboardManager ()
+
+@property (assign, nonatomic) UIResponder *kb_firstResponder;
+
+@end
+
 @implementation DMKeyboardManager
 
 #pragma mark - Shared instance
@@ -25,9 +31,62 @@
 - (id)init {
     self = [super init];
     if (self) {
-        
+        self.kb_firstResponder = nil;
+        [self kb_startObservingDidBeginEditingNotification];
     }
     return self;
+}
+
+- (void)dealloc {
+    [self kb_stopObservingDidBeginEditingNotification];
+}
+
+#pragma mark - Implementation methods
+
+- (void)kb_startObservingDidBeginEditingNotification {
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(kb_textDidBeginEditing:)
+                                                 name:UITextFieldTextDidBeginEditingNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(kb_textDidBeginEditing:)
+                                                 name:UITextViewTextDidBeginEditingNotification
+                                               object:nil];
+}
+
+- (void)kb_stopObservingDidBeginEditingNotification {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UITextFieldTextDidBeginEditingNotification
+                                                  object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UITextViewTextDidBeginEditingNotification
+                                                  object:nil];
+}
+
+#pragma mark - Input Notifications
+
+- (void)kb_textDidBeginEditing:(NSNotification *)notification {
+    self.kb_firstResponder = notification.object;
+
+}
+
+#pragma mark - Resign
+
+- (void)resignKeyboard {
+    UIResponder *responder = self.kb_firstResponder;
+    if (responder == nil) return;
+    
+    self.kb_firstResponder = nil;
+    
+    if (responder.isFirstResponder) {
+        [responder resignFirstResponder];
+    } else {
+        NSLog(@"is not First Responder");
+    }
 }
 
 @end
